aa04b372366e1790fe65e3f3dc820278f33c6a59
niccle27 <niccle27@gmail.com>
2024-11-01 18:04:54 +0100

    [ADD] add merit sort

---
+++ app/utils/merit_manager.py
@@ -0,0 +1,36 @@
+from typing import List, Tuple
+
+from app.schemas import (ApiProductionPlanPayload,
+                         PowerplantType, FuelsCost,
+                         PowerplantData)
+from abc import ABC, abstractmethod
+
+class AbstractCostComputer(ABC):
+    @abstractmethod
+    def compute(self, pp: PowerplantData) -> float:
+        pass
+
+class CostComputer(AbstractCostComputer):
+    def compute(self, pp: PowerplantData, fuels_cost: FuelsCost):
+        cost = None
+        if pp.type == PowerplantType.GASFIRED :
+            cost = fuels_cost.gas / pp.efficiency
+        elif pp.type == PowerplantType.TURBOJET:
+            cost = fuels_cost.kerosine / pp.efficiency
+        elif pp.type == PowerplantType.WINDTURBINE:
+            cost = 0
+        else:
+            raise NotImplementedError()
+        return cost
+
+class MeritSortManager():
+    def __init__(self, cost_computer: AbstractCostComputer):
+        self.cc = cost_computer
+
+    def sort_by_lower_cost_per_MWh(self, payload: ApiProductionPlanPayload)\
+        -> List[Tuple[float, PowerplantData]]:
+        pairs_cost_pp: list = []
+        for pp in payload.powerplants:
+            pairs_cost_pp.append((self.cc.compute(pp), pp))
+        sorted_pairs = sorted(pairs_cost_pp, key= lambda e: e[0])
+        return sorted_pairs
