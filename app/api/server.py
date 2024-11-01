5c0495f0823d5a629a2205a220ab16233463cb57
niccle27 <niccle27@gmail.com>
2024-11-01 19:12:10 +0100

    [IMP] bind planner in api

--- app/api/server.py
+++ app/api/server.py
@@ -1,5 +1,8 @@
 from fastapi import APIRouter, FastAPI
 import uvicorn
+from app.controller.load_planner import LoadPlannerGreedy
+from app.utils.parser import ApiProductionPlanPayloadParser
+from app.utils.merit_manager import MeritSortManager
 
 class ApiServer():
     def __init__(self):
@@ -17,5 +20,14 @@ class ApiServer():
 
         @router.post("/productionplan")
         def plan_production(payload: dict):
-            return []
+            parser = ApiProductionPlanPayloadParser()
+            merit_sorter = MeritSortManager()
+            planner = LoadPlannerGreedy()
+            pp_data = parser.parse(payload)
+            sorted_pairs_pp = merit_sorter.sort_by_lower_cost_per_MWh(pp_data)
+            sorted_powerplants = [e[1] for e in sorted_pairs_pp]
+            load_production_plan = planner.plan(pp_data.load, sorted_powerplants)
+            serialized_lpp = [e.model_dump() for e in load_production_plan]
+            return serialized_lpp
+
         return router
