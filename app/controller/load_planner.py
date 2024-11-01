479225f85a27dce6e9238e48e90924e6eb09af64
niccle27 <niccle27@gmail.com>
2024-11-01 19:00:22 +0100

    [IMP] add implementation for greedy load planner

--- app/controller/load_planner.py
+++ app/controller/load_planner.py
@@ -1,10 +1,33 @@
 from typing import List
-from app.schemas import PowerplantData, ApiProductionPlanPayload
+from app.schemas import PowerplantData, ProductionPlanningData
 
 class LoadPlannerGreedy():
     def __init__(self):
         pass
 
     def plan(self, load: float, powerplants: List[PowerplantData])\
-        -> List[ApiProductionPlanPayload]:
-            return []
+        -> List[ProductionPlanningData]:
+            total_load = load
+            remaining_load = total_load
+            production_plan = []
+            for pp in powerplants:
+                if remaining_load == 0:
+                    production_plan.append(ProductionPlanningData(
+                        name=pp.name,
+                        p=0
+                    ))
+                elif pp.pmin < remaining_load < pp.pmax:
+                    production_plan.append(ProductionPlanningData(
+                        name=pp.name,
+                        p=remaining_load
+                    ))
+                    remaining_load = 0
+                elif pp.pmin > remaining_load:
+                    continue
+                elif remaining_load > pp.pmax:
+                    production_plan.append(ProductionPlanningData(
+                        name=pp.name,
+                        p=pp.pmax
+                    ))
+                    remaining_load -= pp.pmax
+            return production_plan
