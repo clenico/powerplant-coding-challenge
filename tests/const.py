d72e42c92034c5e6d348d7d15345fe244d7d508c
niccle27 <niccle27@gmail.com>
2024-11-01 21:34:16 +0100

    [FIX] resolve path to example as absolute

---
+++ tests/const.py
@@ -0,0 +1,7 @@
+from pathlib import Path
+
+
+EXAMPLE_DIR = Path(__file__).parent / "example_payloads"
+
+PATH_PAYLOAD_3 = EXAMPLE_DIR / "payload3.json"
+PATH_RESPONSE_3 = EXAMPLE_DIR / "response3.json"
